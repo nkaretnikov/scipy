d0f36f3149bc9ff122f546faa29a568ca6c77dc0
Nikita Karetnikov <nkaretnikov@quansight.com>
2022-06-22 13:15:53 +0000

    MAINT: add base pointer and size to `_ComputeFT`

--- scipy/ndimage/src/ni_morphology.c
+++ scipy/ndimage/src/ni_morphology.c
@@ -941,11 +941,11 @@ static void _VoronoiFT(char *pf, npy_intp len, npy_intp *coor, int rank,
 
 
 /* Recursive feature transform */
-static void _ComputeFT(char *pi, char *pf, npy_intp *ishape,
-                       const npy_intp *istrides, const npy_intp *fstrides,
-                       int rank, int d, npy_intp *coor, npy_intp **f,
-                       npy_intp *g, PyArrayObject *features,
-                       const npy_double *sampling)
+static void _ComputeFT(char *pi, char *pf, char *pf_base, npy_intp pf_size,
+                       npy_intp *ishape, const npy_intp *istrides,
+                       const npy_intp *fstrides, int rank, int d,
+                       npy_intp *coor, npy_intp **f, npy_intp *g,
+                       PyArrayObject *features, const npy_double *sampling)
 {
     npy_intp kk;
     npy_intp jj;
@@ -976,8 +976,8 @@ static void _ComputeFT(char *pi, char *pf, npy_intp *ishape,
 
         for(jj = 0; jj < ishape[d]; jj++) {
             coor[d] = jj;
-            _ComputeFT(pi, tf, ishape, istrides, fstrides, rank, d - 1, coor, f,
-                                 g, features, sampling);
+            _ComputeFT(pi, tf, pf_base, pf_size, ishape, istrides, fstrides,
+                       rank, d - 1, coor, f, g, features, sampling);
             pi += istrides[d];
             tf += fstrides[d + 1];
         }
@@ -1012,12 +1012,15 @@ int NI_EuclideanFeatureTransform(PyArrayObject* input,
     int ii;
     npy_intp coor[NPY_MAXDIMS], mx = 0, jj;
     npy_intp *tmp = NULL, **f = NULL, *g = NULL;
-    char *pi, *pf;
+    char *pi = NULL, *pf = NULL, *pf_base = NULL;
+    npy_intp pf_size = 0;
     npy_double *sampling = sampling_arr ? ((void *)PyArray_DATA(sampling_arr)) : NULL;
     NPY_BEGIN_THREADS_DEF;
 
     pi = (void *)PyArray_DATA(input);
     pf = (void *)PyArray_DATA(features);
+    pf_base = pf;
+    pf_size = PyArray_NBYTES(features);
     for (ii = 0; ii < PyArray_NDIM(input); ii++) {
         coor[ii] = 0;
         if (PyArray_DIMS(input)[ii] > mx) {
@@ -1039,9 +1042,10 @@ int NI_EuclideanFeatureTransform(PyArrayObject* input,
 
     /* First call of recursive feature transform */
     NPY_BEGIN_THREADS;
-    _ComputeFT(pi, pf, PyArray_DIMS(input), PyArray_STRIDES(input),
-               PyArray_STRIDES(features), PyArray_NDIM(input),
-               PyArray_NDIM(input) - 1, coor, f, g, features, sampling);
+    _ComputeFT(pi, pf, pf_base, pf_size, PyArray_DIMS(input),
+               PyArray_STRIDES(input), PyArray_STRIDES(features),
+               PyArray_NDIM(input), PyArray_NDIM(input) - 1, coor, f, g,
+               features, sampling);
     NPY_END_THREADS;
 
  exit:
